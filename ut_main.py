f92b6b97dafa7d05140173e89602f4dd5ff00724
Magdalena Huk <Magdalena.Huk@brightone.pl>
2018-01-11 13:34:25 +0100

    Add test for menu_content

--- ut_main.py
+++ ut_main.py
@@ -4,6 +4,7 @@ from unittest import main as ut_main
 from unittest import TestCase
 from unittest.mock import patch
 from main import FileManager
+from main import Menu
 import shutil, tempfile
 
 
@@ -27,6 +28,22 @@ class TestFileManager(TestCase):
         is_file_mock.assert_called_once_with('2017.txt')
         open_mock.assert_called_once_with('2017.txt', 'r+')
         self.assertNotEqual(None, res)
+
+class TestMenu(TestCase):
+
+    @patch('builtins.input', return_value='3')
+    def test_exit_menu(self,input_mock):
+        menu = Menu()
+        res = menu.menu_content()
+        input_mock.assert_called_once()
+        self.assertEqual(None, res)
+
+    @patch('builtins.input', return_value='as')
+    def test_wrong_number(self, input_mock):
+        menu = Menu()
+        res = menu.menu_content()
+        input_mock.assert_called_once()
+        self.assertRaises(TypeError, res)
 
 if __name__ == "__main__":
     ut_main()
